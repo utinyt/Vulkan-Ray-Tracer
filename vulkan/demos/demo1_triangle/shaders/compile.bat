fa2704c5cd918cc9418331e684faec3e603e71bb
jooho.jeong <jooho556@gmail.com>
2021-11-06 16:55:54 -0700

    Erased rasterizer demos

--- vulkan/demos/demo1_triangle/shaders/compile.bat
+++
@@ -1,5 +0,0 @@
-..\..\glslc.exe shader.vert -o vert.spv
-..\..\glslc.exe shader.frag -o frag.spv
-..\..\glslc.exe phong.vert -o phong_vert.spv
-..\..\glslc.exe phong.frag -o phong_frag.spv
-pause
--- vulkan/demos/demo3_phong_reflection/shaders/compile.bat
+++
@@ -1,3 +0,0 @@
-..\..\glslc.exe phong.vert -o phong_vert.spv
-..\..\glslc.exe phong.frag -o phong_frag.spv
-pause
